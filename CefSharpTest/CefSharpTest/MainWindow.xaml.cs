73845ef6e7cfdf5a701cb19d761c85371526ca81
pvaughn <pvaughn@hrsi.com>
2021-02-05 14:58:28 -0600

    Cefsharp
    committing cefsharp test application

---
+++ CefSharpTest/CefSharpTest/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CefSharpTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception e)
+            {
+                WriteToTextFile("log.txt", e.ToString());
+            }
+        }
+
+        public static void WriteToTextFile(string file, object text)
+        {
+            var toToAdd = text?.ToString() ?? "null";
+            var docPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "C Projects");
+            var internalPath = Path.Combine(docPath, file);
+            File.AppendAllText(internalPath, toToAdd);
+        }
+    }
+}
